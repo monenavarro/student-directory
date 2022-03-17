9c1d2d3883295ff11ad0372ab70959152f22bf8d
Monica Navarro <me.navarro.97@outlook.com>
2022-03-17 11:19:48 +0000

    Save the list of students to csv file

--- directory.rb
+++ directory.rb
@@ -1,44 +1,61 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Display the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # terminates the program
-    else 
-      puts "ERROR! Try again."
-    end
-  end
-end
+@students = [] 
 
 def input_students
   puts "Please enter the names of the students:"
   puts "To finish, just hit the return twice."
-  # create an empty array
-  students = []
-  # get the first name
+  
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Display the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def display_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    display_students
+  when "3"
+    save_students
+  when "9"
+    exit 
+  else 
+    puts "ERROR! Try again."
+  end
 end
 
 def print_header
@@ -46,19 +63,14 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} cunning students."
+def print_footer
+  puts "Overall, we have #{@students.count} cunning students."
 end
 
-# nothing happens until we call the methods
 interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)
